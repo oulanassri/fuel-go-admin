a30e3a9aefe08cdf7966bdad8caf434ffd30f9ce
oulanassri <oulanassri@gmail.com>
2025-07-03 17:03:26 +0200

    Update main.dart to work on windows

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:admin_fuel_go/routes/app_pages.dart';
 import 'package:admin_fuel_go/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
